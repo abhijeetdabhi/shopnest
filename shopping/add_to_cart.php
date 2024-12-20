cf1e1b406c61c74f60050debfae518d8d1ec430e
abhijeetdabhi <abhijeetdabhi3904@gmail.com>
2024-12-20 17:08:34 +0530

    updates

--- shopping/add_to_cart.php
+++ shopping/add_to_cart.php
@@ -15,7 +15,38 @@
         exit;
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tailwind Script  -->
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
+    <!-- alpine CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
+    <!-- Fontawesome Link for Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- link to css -->
+    <link rel="stylesheet" href="">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../src/logo/favIcon.svg">
+
+    <!-- title -->
+    <title>Add To Cart</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 
 include "../include/connect.php";
@@ -80,30 +111,28 @@ if (isset($_GET['product_id'])) {
             ?>
                 <!-- Tailwind Script  -->
                 <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-                <!-- Error -->
-                <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="EpopUp" style="display: none;">
-                    <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <!-- Error -->                
+                <div class="validInfo fixed top-3 left-1/2 transform -translate-x-1/2 w-max border-t-4 rounded-lg border-red-500 py-3 px-6 bg-gray-800 z-50" id="popUp" style="display: none;">
+                    <div class="flex items-center m-auto justify-center text-sm text-red-400">
                         <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                         </svg>
                         <span class="sr-only">Info</span>
-                        <div>
-                            <span class="font-medium">Item Already in Cart.</span>
-                        </div>
+                        <div class="capitalize font-medium">This Product Already In Cart.</div>
                     </div>
                 </div>
     
                 <script>
-                    let EpopUp = document.getElementById('EpopUp');
-    
-                    EpopUp.style.display = 'flex';
-                    EpopUp.style.opacity = '100';
-    
+                    let popUp = document.getElementById('popUp');
+
+                    popUp.style.display = 'flex';
+                    popUp.style.opacity = '100';
+                    
                     setTimeout(() => {
-                        EpopUp.style.display = 'none';
-                        EpopUp.style.opacity = '0';
-                        window.location.href = '/shopping/cart.php';
-                    }, 1500);
+                        popUp.style.display = 'none';
+                        popUp.style.opacity = '0';
+                        window.location.href = '../shopping/cart.php';
+                    }, 1800);
                 </script>
             <?php
         } else {
@@ -113,30 +142,37 @@ if (isset($_GET['product_id'])) {
             ?>
                 <!-- Tailwind Script  -->
                 <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-                <!-- Successfully -->
-                <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-                    <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                <!-- loader -->
+                <div id="loader" class="flex-col gap-4 w-full flex items-center justify-center bg-black/30 fixed top-0 h-full backdrop-blur-sm z-40" style="display: none;">
+                    <div class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-gray-700 rounded-full">
+                        <div class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-gray-900 rounded-full"></div>
+                    </div>
+                </div>
+                <!-- Successfully message container -->
+                <div class="validInfo fixed top-3 left-1/2 transform -translate-x-1/2 w-max border-t-4 m-auto rounded-lg border-green-400 py-3 px-6 bg-gray-800 z-[100]" id="SpopUp" style="display: none;">
+                    <div class="flex items-center m-auto justify-center text-sm text-green-400" role="alert">
                         <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                         </svg>
                         <span class="sr-only">Info</span>
-                        <div>
-                            <span class="font-medium">Product Added to Cart Successfully.</span>
-                        </div>
+                        <div class="capitalize font-medium">Product Added to Cart Successfully.</div>
                     </div>
                 </div>
     
                 <script>
-                    let popUp = document.getElementById('popUp');
-    
-                    popUp.style.display = 'flex';
-                    popUp.style.opacity = '100';
-    
+                    let loader = document.getElementById('loader');
+                    let body = document.body;
+
+                    loader.style.display = 'flex';
+                    body.style.overflow = 'hidden';
+
+                    let SpopUp = document.getElementById('SpopUp');
+
                     setTimeout(() => {
-                        popUp.style.display = 'none';
-                        popUp.style.opacity = '0';
-                        window.location.href = '/shopping/cart.php';
-                    }, 1500);
+                        SpopUp.style.display = 'flex';
+                        SpopUp.style.opacity = '100';
+                        window.location.href = '../shopping/cart.php';
+                    }, 2000);
                 </script>
             <?php
         }
--- shopping/remove_from_cart.php
+++ shopping/remove_from_cart.php
@@ -42,29 +42,37 @@
             <!-- Tailwind Script  -->
             <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
             
-            <!-- Successfully -->
-            <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-                <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" >
-                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>
-                    <span class="sr-only">Info</span>
-                    <div>
-                        <span class="font-medium">Item remove successfully.</span>
-                    </div>
+            <!-- loader -->
+            <div id="loader" class="flex-col gap-4 w-full flex items-center justify-center bg-black/30 fixed top-0 h-full backdrop-blur-sm z-40" style="display: none;">
+                <div class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-gray-700 rounded-full">
+                    <div class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-gray-900 rounded-full"></div>
                 </div>
             </div>
-
+            <!-- Successfully message container -->
+            <div class="validInfo fixed top-3 left-1/2 transform -translate-x-1/2 w-max border-t-4 m-auto rounded-lg border-green-400 py-3 px-6 bg-gray-800 z-[100]" id="SpopUp" style="display: none;">
+                <div class="flex items-center m-auto justify-center text-sm text-green-400" role="alert">
+                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                    </svg>
+                    <span class="sr-only">Info</span>
+                    <div class="capitalize font-medium">Product Added to Cart Successfully.</div>
+                </div>
+            </div>
+    
             <script>
-            
-                let popUp = document.getElementById('popUp');
+                let loader = document.getElementById('loader');
+                let body = document.body;
 
-                popUp.style.display = 'flex';
-                popUp.style.opacity = '100';
+                loader.style.display = 'flex';
+                body.style.overflow = 'hidden';
+
+                let SpopUp = document.getElementById('SpopUp');
 
                 setTimeout(() => {
-                    popUp.style.display = 'none';
-                    popUp.style.opacity = '0';
-                    window.location.href = 'cart.php'
-                }, 1500);
+                    SpopUp.style.display = 'flex';
+                    SpopUp.style.opacity = '100';
+                    window.location.href = '../shopping/cart.php';
+                }, 2000);
             </script>
         <?php
         }
