6538097f08936da4fbf53b511f761a211c620c0a
abhijeetdabhi <abhijeetdabhi3904@gmail.com>
2025-02-04 15:04:46 +0530

    chnage suggestion file

--- search/suggestion.php
+++ search/suggestion.php
@@ -52,7 +52,7 @@ if (isset($_POST["query"])) {
     $keywords = explode(' ', $mydata);
 
     // Build query for matching keywords (AND logic)
-    $query = "SELECT * FROM products";
+    $query = "SELECT * FROM products WHERE ";
     $queryParts = [];
     foreach ($keywords as $keyword) {
         $queryParts[] = "keywords LIKE '%" . $keyword . "%'"; // Match each keyword
